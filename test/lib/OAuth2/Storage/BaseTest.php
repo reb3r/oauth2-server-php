2e6a0b466daaa1d353e07873fb5fa3452c66fe43
Christian Reber <chris@creber.de>
2018-07-05 21:50:22 +0200

    Remove some storage implementations

--- test/lib/OAuth2/Storage/BaseTest.php
+++ test/lib/OAuth2/Storage/BaseTest.php
@@ -12,12 +12,7 @@ abstract class BaseTest extends TestCase
         $sqlite = Bootstrap::getInstance()->getSqlitePdo();
         $mysql = Bootstrap::getInstance()->getMysqlPdo();
         $postgres = Bootstrap::getInstance()->getPostgresPdo();
-        $mongo = Bootstrap::getInstance()->getMongo();
-        $mongoDb = Bootstrap::getInstance()->getMongoDB();
         $redis = Bootstrap::getInstance()->getRedisStorage();
-        $cassandra = Bootstrap::getInstance()->getCassandraStorage();
-        $dynamodb = Bootstrap::getInstance()->getDynamoDbStorage();
-        $couchbase = Bootstrap::getInstance()->getCouchbase();
 
         /* hack until we can fix "default_scope" dependencies in other tests */
         $memory->defaultScope = 'defaultscope1 defaultscope2';
@@ -27,12 +22,7 @@ abstract class BaseTest extends TestCase
             array($sqlite),
             array($mysql),
             array($postgres),
-            array($mongo),
-            array($mongoDb),
             array($redis),
-            array($cassandra),
-            array($dynamodb),
-            array($couchbase),
         );
     }
 }
